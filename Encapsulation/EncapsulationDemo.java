8be91adeb36de7910f94647c468a7bc7db09ba44
Saran2703 <91609924+Saran2703@users.noreply.github.com>
2022-11-23 15:55:13 +0530

    Update EncapsulationDemo.java

--- Encapsulation/EncapsulationDemo.java
+++ Encapsulation/EncapsulationDemo.java
@@ -1,41 +1,25 @@
-class EncapsulationDemo{
-    private int ssn;
-    private String empName;
-    private int empAge;
 
-    //Getter and Setter methods
-    public int getEmpSSN(){
-        return ssn;
+class Name {
+ 
+    private int age; // Private is using to hide the data
+ 
+    public int getAge() { return age; } // getter
+ 
+    public void setAge(int age)
+    {
+        this.age = age;
+    } // setter
+}
+ 
+class GFG {
+    public static void main(String[] args)
+    {
+ 
+        Name n1 = new Name();
+ 
+        n1.setAge(19);
+ 
+        System.out.println("The age of the person is: "
+                           + n1.getAge());
     }
-
-    public String getEmpName(){
-        return empName;
-    }
-
-    public int getEmpAge(){
-        return empAge;
-    }
-
-    public void setEmpAge(int newValue){
-        empAge = newValue;
-    }
-
-    public void setEmpName(String newValue){
-        empName = newValue;
-    }
-
-    public void setEmpSSN(int newValue){
-        ssn = newValue;
-    }
-
-
-    public static void main(String args[]){
-         EncapsulationDemo obj = new EncapsulationDemo();
-         obj.setEmpName("Mario");
-         obj.setEmpAge(32);
-         obj.setEmpSSN(112233);
-         System.out.println("Employee Name: " + obj.getEmpName());
-         System.out.println("Employee SSN: " + obj.getEmpSSN());
-         System.out.println("Employee Age: " + obj.getEmpAge());
-    } 
 }
