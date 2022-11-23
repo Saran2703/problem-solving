8b7aeca3d1323223cadb61581ab3659d65511314
Saran2703 <91609924+Saran2703@users.noreply.github.com>
2022-11-23 15:51:10 +0530

    Update Main1.java

--- Abstraction/Main1.java
+++ Abstraction/Main1.java
@@ -1,25 +1,73 @@
-// Abstract class
-abstract class Animal {
-  // Abstract method (does not have a body)
-  public abstract void animalSound();
-  // Regular method
-  public void sleep() {
-    System.out.println("Zzz");
-  }
+/abstract class Shape {
+    String color;
+ 
+    // these are abstract methods
+    abstract double area();
+    public abstract String toString();
+ 
+    // abstract class can have the constructor
+    public Shape(String color)
+    {
+        System.out.println("Shape constructor called");
+        this.color = color;
+    }
+ 
+    // this is a concrete method
+    public String getColor() { return color; }
 }
-
-// Subclass (inherit from Animal)
-class Pig extends Animal {
-  public void animalSound() {
-    // The body of animalSound() is provided here
-    System.out.println("The pig says: wee wee");
-  }
+class Circle extends Shape {
+    double radius;
+ 
+    public Circle(String color, double radius)
+    {
+ 
+        // calling Shape constructor
+        super(color);
+        System.out.println("Circle constructor called");
+        this.radius = radius;
+    }
+ 
+    @Override double area()
+    {
+        return Math.PI * Math.pow(radius, 2);
+    }
+ 
+    @Override public String toString()
+    {
+        return "Circle color is " + super.getColor()
+            + "and area is : " + area();
+    }
 }
-
-class Main1 {
-  public static void main(String[] args) {
-    Pig myPig = new Pig(); // Create a Pig object
-    myPig.animalSound();
-    myPig.sleep();
-  }
+class Rectangle extends Shape {
+ 
+    double length;
+    double width;
+ 
+    public Rectangle(String color, double length,
+                     double width)
+    {
+        // calling Shape constructor
+        super(color);
+        System.out.println("Rectangle constructor called");
+        this.length = length;
+        this.width = width;
+    }
+ 
+    @Override double area() { return length * width; }
+ 
+    @Override public String toString()
+    {
+        return "Rectangle color is " + super.getColor()
+            + "and area is : " + area();
+    }
+}
+public class Test {
+    public static void main(String[] args)
+    {
+        Shape s1 = new Circle("Red", 2.2);
+        Shape s2 = new Rectangle("Yellow", 2, 4);
+ 
+        System.out.println(s1.toString());
+        System.out.println(s2.toString());
+    }
 }
